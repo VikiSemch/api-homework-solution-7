80aa132a1f0edd40df34084dbdddb96e94422ad2
VikiSemch <108093976+VikiSemch@users.noreply.github.com>
2022-08-13 16:04:02 +0200

    s

--- src/app.js
+++ src/app.js
@@ -31,12 +31,14 @@ function showCTD(response) {
   let HumidityElement = document.querySelector("#Humidity");
   let WindElement = document.querySelector("#Wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
   citynameElement.innerHTML = response.data.name;
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   descritionElement.innerHTML = response.data.weather[0].description;
   HumidityElement.innerHTML = response.data.main.humidity;
   WindElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML =  formatDate(response.data.dt * 1000);
+  iconElement.setAttribute("src",`http://openweathermap.org/img/wn/10d@2x.png`);
 }
 
 let apiKey = "435bb05a3bd8ac1e2c4d7c0df7d0cd4b";
